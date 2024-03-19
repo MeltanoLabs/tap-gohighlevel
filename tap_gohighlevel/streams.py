d3bf7b98c982cea48c556ee66b30e43d16d73cdd
Pat Nadolny <patnadolny@gmail.com>
2024-03-19 18:49:15 -0400

    add more properties to contact stream

--- tap_gohighlevel/streams.py
+++ tap_gohighlevel/streams.py
@@ -28,15 +28,15 @@ class ContactsStream(GoHighLevelStream):
     replication_key = "dateUpdated"
 
     schema = th.PropertiesList(
-        # In Example
         th.Property("id", th.StringType),
         th.Property("locationId", th.StringType),
         th.Property("businessId", th.StringType),
         th.Property("attributions", th.ArrayType(th.ObjectType())),
         th.Property("followers", th.StringType),
         th.Property("email", th.StringType),
-        th.Property("timezone", th.StringType),
         th.Property("country", th.StringType),
+        th.Property("dndSettings", th.ObjectType()),
+        th.Property("additionalEmails", th.ArrayType(th.StringType)),
         th.Property("source", th.StringType),
         th.Property("dateAdded", th.DateTimeType),
         th.Property(
@@ -49,15 +49,12 @@ class ContactsStream(GoHighLevelStream):
             ),
         ),
         th.Property("tags", th.ArrayType(th.StringType)),
-
-        # Other
         th.Property("phone", th.StringType),
         th.Property("lastName", th.StringType),
         th.Property("firstName", th.StringType),
         th.Property("contactName", th.StringType),
         th.Property("companyName", th.StringType),
         th.Property("type", th.StringType),
-        th.Property("source", th.StringType),
         th.Property("dnd", th.BooleanType),
         th.Property("assignedTo", th.StringType),
         th.Property("city", th.StringType),
